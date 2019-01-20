5cc587b3c92cdff75dab56007d745364e7567467
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-20 13:46:56 -0600

    changed url in apimanager.js to the one deployed

--- src/components/managers/APIManagerOg.js
+++ src/components/managers/APIManagerOg.js
@@ -1,4 +1,4 @@
-const LocalRemoteURL = "https://heuristic-mestorf-ea2618.netlify.com"
+const LocalRemoteURL = "http://localhost:8088"
 const GeolocationRemoteURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDOEBqiYykHzoCJyKAij9f2UwaF-DxtuBs"
 // const MapsRemoteURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restrooms&location=${36.1627},%20${-86.7816}&radius=10000&key=AIzaSyDOEBqiYykHzoCJyKAij9f2UwaF-DxtuBs`
 // const MapsRemoteURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restrooms" //worked on postman
