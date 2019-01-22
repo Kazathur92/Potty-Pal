356ac7de084a1e592799e0f71a94bcd320235e4f
Alfonso Miranda <Gabmin21@hotmail.com>
2019-01-21 23:03:38 -0600

    test

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ class App extends Component {
         const { latitude, longitude } = position.coords;
 
         BathroomManager.BathroomManagerGetAll(`&location=${latitude},%20${longitude}&radius=10000`)
-        .then(allBathrooms => this.setState({ bathrooms: allBathrooms.results }))
+        .then(allBathrooms => console.log(allBathrooms))
+          // this.setState({ bathrooms: allBathrooms.results }))
 
         this.setState({
           userLocation: { lat: latitude, lng: longitude },
